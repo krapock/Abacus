028dfdc7a053613bde71cff84ca5ca5ee74c3d28
Damien VS <krapock@gmail.com>
2021-05-25 17:41:34 +0200

    added grid generation + pearl click animation

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 a configurable abacus web page
 
 
+## Configuration
+ 
+	data-direction : "vertical", "horizontal" default : vertical
+	data-rows : "5x4, 5x2"  default : "6x4"
+	data-counter : "true", "false. default : true;
+
 ## Autorefresh
 Launch a simple static webserver (eg. "python3 -m http.server") and the page should automagically refresh on change.
---
+++ js/abacus.js
@@ -0,0 +1,128 @@
+(function(){
+
+	function Abacus(htmlRoot){
+		this.htmlRoot=htmlRoot;
+		this.attributes={
+			direction:'vertical',
+			rows:'6x4',
+			counter:true,
+		}
+		
+		this.loadAttributes=()=>{
+			this.attributes = {...this.attributes, ...this.htmlRoot.dataset}
+			this.attributes.rows = this.extendRowsNotation(this.attributes.rows)
+		}
+		
+		this.extendRowsNotation = base => {
+			return base.replace(/\s+/g,' ').split(',')
+				.map( line => line.trim().split(' ')
+					.flatMap( number => {
+						if(number.match(/^\d+$/)){ return Number(number) }
+						else if(number.match(/^\d+x\d+$/)){
+							let [rep,val]=number.split('x')
+							return new Array(Number(rep)).fill(Number(val))
+						} 
+						throw "wrong rows notation"
+					}))
+		}
+		
+		this.buildHtmlContent = ()=>{
+			removeClass(this.htmlRoot,'vertical horizontal')
+			addClass(this.htmlRoot,this.attributes.direction)
+			this.attributes.rows.forEach( this.addColumnGroup )
+		}
+		
+		this.addColumnGroup = colGroupLength => {
+			let colGroup = document.createElement('div')
+			colGroup.setAttribute('class','column-group')
+			
+			colGroupLength.forEach( columnLength => this.addColumn(colGroup,columnLength) )
+			
+			this.htmlRoot.appendChild(colGroup)
+		}
+		this.addColumn = (columnGroupNode, length) => {
+			let column = document.createElement('div')
+			column.setAttribute('class','column')
+			
+			let spacer = document.createElement('div')
+			spacer.setAttribute('class','spacer active')
+			column.appendChild(spacer)
+			
+			for(let i=0;i<length;i++){
+			 	let pearl = document.createElement('div')
+			 	pearl.setAttribute('class','pearl')
+				pearl.addEventListener('click', evt => this.togglePearl(evt.target))
+				column.appendChild(pearl)
+			 	let spacer = document.createElement('div')
+			 	spacer.setAttribute('class','spacer')
+				column.appendChild(spacer)
+			}
+			
+			columnGroupNode.appendChild(column)
+		}
+		
+		this.togglePearl = pearl => {
+			console.log(pearl)
+			let siblings = [...pearl.parentNode.children];
+			let foundPearl = false
+			let foundActive = false
+			
+			for(let i=0;i<siblings.length;i++){
+				let element = siblings[i]
+				
+				if(!foundPearl && element == pearl){
+					foundPearl=true
+					if(foundActive){
+						addClass(siblings[i+1],'active')
+						break;
+					}else{
+						addClass(siblings[i-1],'active')
+					}
+				}
+				if( hasClass(element,'spacer') ){
+					if( hasClass(element,'active') ){
+						foundActive=true
+						removeClass(element,'active')
+					}
+				}
+			}
+		}
+		
+		this.init=()=>{
+			try{
+				truncate(htmlRoot)
+				this.loadAttributes()
+				this.buildHtmlContent()
+			}catch(e){
+				console.warn("problem generating the abacus : "+e)
+			}
+		}
+		this.init();
+	}
+	
+	//dom utilities
+	const truncate = node=>{
+		while(node.hasChildNodes()){ node.firstChild.remove() }
+	}
+	const removeClass = (node, classes) =>{
+		node.setAttribute('class', classes.split(" ")
+			.reduce( (attr, clazz) => attr.replace(clazz,"").trim() , node.getAttribute("class"))
+			.replace(/\s+/g," ")
+		)
+	}
+	const addClass = (node, classes) =>{
+		node.setAttribute('class', (node.getAttribute("class")+" "+classes).replace(/\s+/g," "))
+	}
+	const hasClass = (node, clazz) => node.getAttribute("class").split(/\s+/).includes(clazz)
+	
+	
+	
+	//boostrap : launch after the page content is loaded
+	document.addEventListener('DOMContentLoaded', (event) => {
+		let abacuses = document.getElementsByClassName("abacus")
+		for( x=0; x<abacuses.length; x++){
+			new Abacus(abacuses.item(x))
+		}
+	});
+
+})();
